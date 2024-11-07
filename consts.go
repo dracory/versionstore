1ddc48e5d917b6cfa9ed562609c1d805ef627aff
Milan Lesichkov <lesichkovm@gmail.com>
2024-11-07 20:23:59 +0000

    Add files via upload

---
+++ consts.go
@@ -0,0 +1,10 @@
+package versionstore
+
+const COLUMN_CONTENT = "content"
+const COLUMN_CREATED_AT = "created_at"
+const COLUMN_ENTITY_ID = "entity_id"
+const COLUMN_ENTITY_TYPE = "entity_type"
+const COLUMN_ID = "id"
+const COLUMN_REVISION = "revision"
+const COLUMN_SOFT_DELETED_AT = "soft_deleted_at"
+const COLUMN_UPDATED_AT = "updated_at"
